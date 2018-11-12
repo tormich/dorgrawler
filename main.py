eeaffd7bc92dca96b2b95e466da6edb7c18a7324
@tormich <gmail@tormich.name>
2018-11-12 17:29:37 +0200

    hmm

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import os
+
 import requests
 
 session = requests.session()
 session.proxies = {}
-session.proxies['http'] = 'socks5h://127.0.0.1:9050'
-session.proxies['https'] = 'socks5h://:9050'
+session.proxies['http'] = os.getenv('http_proxy', 'socks5h://127.0.0.1:9050')
+session.proxies['https'] = os.getenv('https_proxy', 'socks5h://127.0.0.1:9050')
 
 print('requesting site')
 r = session.get('http://nzxj65x32vh2fkhk.onion/all')
