6822d94b0377c76a6fc49e087bb4575d76008a96
@tormich <gmail@tormich.name>
2018-11-12 16:04:48 +0200

    for now url will be here

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import requests
 
 session = requests.session()
 session.proxies = {
-    'http': 'socks5h://localhost:9150',
-    'https': 'socks5h://localhost:9150'}
+    'http': 'socks5h://localhost:9100',
+    'https': 'socks5h://localhost:9100'}
 
 print('requesting site')
 r = session.get('http://nzxj65x32vh2fkhk.onion/all')
