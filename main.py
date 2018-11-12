2beb2ffde2b3bce0b27a4d8918ed09319bce2131
@tormich <gmail@tormich.name>
2018-11-12 16:47:58 +0200

    i need a linux machine. this is madness

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import requests
 
 session = requests.session()
-session.proxies = {
-    'http': 'socks5h://localhost:9050',
-    'https': 'socks5h://localhost:9050'}
+session.proxies = {}
+session.proxies['http'] = 'socks5h://127.0.0.1:9050'
+session.proxies['https'] = 'socks5h://:9050'
 
 print('requesting site')
 r = session.get('http://nzxj65x32vh2fkhk.onion/all')
